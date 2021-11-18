a82fa549c241e78de711c9a559166399898e4335
Alexey Strakh <alstrakh@microsoft.com>
2021-11-18 12:42:27 -0800

    initial solution structure

---
+++ MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+using System;
+
+namespace MauiTelerikControlsDisappear
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            TelerikGrid.ItemsSource = new[]
+            {
+                new { Col1 = "AAA", Col2 = 50 },
+                new { Col1 = "BBB", Col2 = 55 },
+                new { Col1 = "CCC", Col2 = 60 },
+            };
+        }
+
+        private void OnButtonClick(object sender, EventArgs e)
+        {
+            Container.Children.Remove(TelerikGrid);
+            Container.Children.Add(TelerikGrid);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Maui Telerik Controls Disappearing issue
+
+MAUI sample to reproduce an issue with disappearing telerik grid on re-add a control to a parent layout
